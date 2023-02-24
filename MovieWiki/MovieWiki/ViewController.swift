b970c1460f58f771ae92c3a73cafc6d7353896de
Judy <joo00ee@naver.com>
2023-02-24 10:56:17 +0900

    upload xcode file

---
+++ MovieWiki/MovieWiki/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieWiki
+//
+//  Created by 김주영 on 2023/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
